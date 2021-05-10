b52bd49cdde6e4c0931a214d07ecd9a02265912d
Jessica Wiersma <jesswiersma@gmail.com>
2021-05-09 21:02:16 -0400

    working fade

--- src/Components/PokeFetch/timer.js
+++ src/Components/PokeFetch/timer.js
@@ -2,19 +2,19 @@ import React from "react";
 
 export default function Timer() {
   const [timer, setTimer] = React.useState(10);
-  const id =React.useRef(null);
-  const clear=()=>{
+  const id = React.useRef(null);
+  const clear = () => {
   window.clearInterval(id.current)
 }
-  React.useEffect(()=>{
-     id.current=window.setInterval(()=>{
-      setTimer((time)=>time-1)
+  React.useEffect(() => {
+     id.current = window.setInterval(() => {
+      setTimer((time) => time-1)
     },1000)
-    return ()=>clear();
+    return () => clear();
   },[])
 
-  React.useEffect(()=>{
-    if(timer===0){
+  React.useEffect(() => {
+    if(timer === 0){
       clear()
     }
 
